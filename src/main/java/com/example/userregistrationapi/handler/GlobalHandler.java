fb3432033a59bfd7cfc380e59eef6343c0d298e8
Tyler Sargent <tyler.sargent@outlook.com>
2023-08-10 08:03:21 -0300

    Add proper error responses for param validation

--- src/main/java/com/example/userregistrationapi/handler/GlobalHandler.java
+++ src/main/java/com/example/userregistrationapi/handler/GlobalHandler.java
@@ -2,8 +2,8 @@ package com.example.userregistrationapi.handler;
 
 import javax.validation.ConstraintViolationException;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalHandler {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalHandler.class);
+    // private static final Logger LOGGER = LoggerFactory.getLogger(GlobalHandler.class);
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<String> handlyViolationException(ConstraintViolationException e) {
